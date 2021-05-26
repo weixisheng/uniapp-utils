399f3edf90edc92e289b7265f317a2bb01008222
hishion <956656628@qq.com>
2021-05-26 11:11:01 +0800

    build: 优化构建

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp')
-const babel = require('gulp-babel')
-const rename = require('gulp-rename')
-const uglify = require('gulp-uglify')
+const webpack = require('webpack-stream')
 
 gulp.task('default', function () {
-  return gulp.src('./src/index.js')
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
+  return gulp
+    .src(['./src/index.js'])
+    .pipe(
+      webpack({
+        output: {
+          filename: 'index.min.js',
+          library: `@hishion_wei/uniapp-utils`,
+          libraryTarget: 'umd'
+        }
+      })
+    )
     .pipe(gulp.dest('./lib'))
 })
